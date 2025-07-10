4fc40b34d5f35fd2deab2a39b1b9137d9745c178
BaronVonMuller <raphael.muller02@gmail.com>
2025-07-10 11:59:49 +0200

    Alejandra 2

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
   description = "Your new nix config";
 
   inputs = {
-    nixpkgs          .url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable .url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs          .url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-unstable .url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager     .url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    alejandra        .url = "github:kamadorueda/alejandra";
   };
 
   outputs = {
@@ -14,69 +15,68 @@
     nixpkgs,
     nixpkgs-unstable,
     home-manager,
+    alejandra,
     ...
   } @ inputs: let
-    system = "x86_64-linux";
+    systems = ["x86_64-linux" "aarch64-linux"];
 
     myOverlays = [
-      (import ./overlays/open3d.nix) # <- Open3D
+      (import ./overlays/open3d.nix)
       (final: prev: {
-        # <- Brave unstable (exemple)
-        brave-unstable =
-          nixpkgs-unstable.legacyPackages.${prev.system}.brave;
+        brave-unstable = nixpkgs-unstable.legacyPackages.${prev.system}.brave;
       })
     ];
 
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = myOverlays;
-    };
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+
     serviceUser = "algo0010";
-  in {
-    legacyPackages.${system} = pkgs;
-    packages.${system}.default = pkgs.hello;
+  in
+    {
+      formatter =
+        forAllSystems (system:
+          alejandra.packages.${system}.default);
+    }
+    // forAllSystems (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = myOverlays;
+        };
+      in {
+        legacyPackages = pkgs;
 
-    nixosConfigurations.test = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ({
-          config,
-          pkgs,
-          ...
-        }: {
-          nixpkgs.overlays = [
-            (import ./overlays/open3d.nix)
-            (final: prev: {
-              brave-unstable = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.brave;
-            })
+        packages.default = pkgs.hello;
+        devShells.default = pkgs.mkShell {
+          name = "camera-env-test";
+          packages = [
+            pkgs.alejandra
+            (import ./nixos/modules/python-envs.nix {inherit pkgs;}).cameraServerEnv
           ];
-        })
-        ./nixos/configuration.nix
-        ./nixos/modules/server-camera-package.nix
-      ];
-      specialArgs = {
-        inherit inputs self serviceUser;
-      };
-    };
+        };
 
-    homeConfigurations = {
-      "admin@test" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [./home-manager/admin.nix];
-      };
-      "${serviceUser}@test" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        extraSpecialArgs = {inherit serviceUser;};
-        modules = [./home-manager/algo.nix];
-      };
-    };
+        homeConfigurations = {
+          "admin@test" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [./home-manager/admin.nix];
+          };
 
-    devShells.${system}.default = pkgs.mkShell {
-      name = "camera-env-test";
-      packages = [
-        pkgs.alejandra
-        (import ./nixos/modules/python-envs.nix {inherit pkgs;}).cameraServerEnv
-      ];
+          "${serviceUser}@test" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            extraSpecialArgs = {inherit serviceUser;};
+            modules = [./home-manager/algo.nix];
+          };
+        };
+      }
+    )
+    // {
+      nixosConfigurations.test = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux"; # ← ici tu gardes ta machine cible
+        modules = [
+          ({pkgs, ...}: {nixpkgs.overlays = myOverlays;})
+          ./nixos/configuration.nix
+          ./nixos/modules/server-camera-package.nix
+        ];
+        specialArgs = {inherit inputs self serviceUser;};
+      };
     };
-  };
 }
