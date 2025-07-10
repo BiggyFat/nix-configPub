ec0683ece59e229941af62885aae09100958754a
BaronVonMuller <raphael.muller02@gmail.com>
2025-07-10 11:21:43 +0200

    formatted with alejandra

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-# flake.nix (extrait pertinent)
+# /flake.nix
 {
   description = "Your new nix config";
 
@@ -9,67 +9,74 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+    nixpkgs-unstable,
+    home-manager,
+    ...
+  } @ inputs: let
     system = "x86_64-linux";
 
     myOverlays = [
-      (import ./overlays/open3d.nix)           # <- Open3D
-      (final: prev: {                          # <- Brave unstable (exemple)
+      (import ./overlays/open3d.nix) # <- Open3D
+      (final: prev: {
+        # <- Brave unstable (exemple)
         brave-unstable =
           nixpkgs-unstable.legacyPackages.${prev.system}.brave;
       })
     ];
 
-    pkgs = import nixpkgs { 
-      inherit system; 
-      overlays = myOverlays; 
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = myOverlays;
     };
     serviceUser = "algo0010";
-  in
-  {
+  in {
     legacyPackages.${system} = pkgs;
     packages.${system}.default = pkgs.hello;
 
     nixosConfigurations.test = nixpkgs.lib.nixosSystem {
-  system = "x86_64-linux";
-  modules = [
-    # Module inline pour appliquer les overlays avant le reste de la config
-    ({ config, pkgs, ...}: {
-      nixpkgs.overlays = [
-        (import ./overlays/open3d.nix)
-        (final: prev: { 
-          brave-unstable = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.brave;
+      system = "x86_64-linux";
+      modules = [
+        ({
+          config,
+          pkgs,
+          ...
+        }: {
+          nixpkgs.overlays = [
+            (import ./overlays/open3d.nix)
+            (final: prev: {
+              brave-unstable = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.brave;
+            })
+          ];
         })
+        ./nixos/configuration.nix
+        ./nixos/modules/server-camera-package.nix
       ];
-    })
-    ./nixos/configuration.nix
-    ./nixos/modules/server-camera-package.nix
-  ];
-  specialArgs = {
-    inherit inputs self serviceUser;
-  };
-};
-
+      specialArgs = {
+        inherit inputs self serviceUser;
+      };
+    };
 
     homeConfigurations = {
       "admin@test" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-        modules = [ ./home-manager/admin.nix ];
+        modules = [./home-manager/admin.nix];
       };
       "${serviceUser}@test" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs; extraSpecialArgs = { inherit serviceUser; };
-        modules          = [ ./home-manager/algo.nix ];
+        inherit pkgs;
+        extraSpecialArgs = {inherit serviceUser;};
+        modules = [./home-manager/algo.nix];
       };
     };
 
     devShells.${system}.default = pkgs.mkShell {
-      name     = "camera-env-test";
+      name = "camera-env-test";
       packages = [
         pkgs.alejandra
-        (import ./nixos/modules/python-envs.nix { inherit pkgs; }).cameraServerEnv
+        (import ./nixos/modules/python-envs.nix {inherit pkgs;}).cameraServerEnv
       ];
     };
   };
 }
-
